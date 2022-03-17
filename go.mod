d01cb33ec1a28af0a22222fbb9a4a3ef7998bfae
xxxVitoxxx <bird06291994@gmail.com>
2022-03-18 00:05:28 +0800

    add message command

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/gin-contrib/gzip v0.0.5
 	github.com/gin-gonic/gin v1.7.7
 	github.com/spf13/viper v1.10.1
 	gorm.io/driver/mysql v1.3.2
---
+++ pkg/cmd/message.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/xxxVitoxxx/JiaPen-backend/pkg/config"
+	"github.com/xxxVitoxxx/JiaPen-backend/pkg/http/rest"
+	"github.com/xxxVitoxxx/JiaPen-backend/pkg/message"
+	"github.com/xxxVitoxxx/JiaPen-backend/pkg/storage/conn"
+	"github.com/xxxVitoxxx/JiaPen-backend/pkg/storage/mysqlDB"
+)
+
+func init() {
+	rootCmd.AddCommand(messageCmd)
+}
+
+var messageCmd = &cobra.Command{
+	Use:   "message",
+	Short: "message board",
+	Long:  "message board",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Println("Message Service Start")
+		defer func() {
+			if panicErr := recover(); panicErr != nil {
+				log.Println(panicErr)
+			}
+		}()
+
+		err := config.LoadRun()
+		if err != nil {
+			panic(err)
+		}
+
+		r := gin.Default()
+		r.Use(gzip.Gzip(gzip.BestSpeed))
+
+		mysqlDb := conn.CheckConnect()
+
+		// POST
+		messageHandler := rest.NewMessageHandler(
+			message.NewService(
+				mysqlDB.NewMessageRepo(mysqlDb),
+			),
+		)
+		messageHandler.Router(r)
+
+		go func() {
+			if err := r.Run(fmt.Sprintf(":%s", viper.GetString("message.port"))); err != nil {
+				log.Println("Error: ", err)
+			}
+		}()
+
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		srv := &http.Server{
+			Addr:    fmt.Sprintf(":%s", viper.GetString("message.port")),
+			Handler: r,
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Println("Message Service Shutdown...", err)
+		}
+
+		log.Println("Message Service Exiting")
+	},
+}
