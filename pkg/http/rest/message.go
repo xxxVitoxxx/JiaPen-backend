88e543cf27be51b84184749636bd048cc8baf6c7
xxxVitoxxx <bird06291994@gmail.com>
2022-04-03 23:40:34 +0800

    add patch message api handler

--- pkg/http/rest/message.go
+++ pkg/http/rest/message.go
@@ -2,29 +2,36 @@ package rest
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/xxxVitoxxx/JiaPen-backend/pkg/message"
 )
 
+// MessageHandler _
 type MessageHandler struct {
 	s *message.Service
 }
 
+// NewMessageHandler _
 func NewMessageHandler(s *message.Service) *MessageHandler {
 	return &MessageHandler{s}
 }
 
+// Router _
 func (h *MessageHandler) Router(r *gin.Engine) {
 	api := r.Group("/message_api")
 	api.POST("/message", h.CreateMessage)
+	api.PATCH("/message/:message_id", h.UpdateMessage)
 }
 
+// CreateMessage _
 type CreateMessage struct {
 	User    string `json:"user" binding:"required"`
 	Content string `json:"content" binding:"min=1,max=300"`
 }
 
+// CreateMessage _
 func (h *MessageHandler) CreateMessage(c *gin.Context) {
 	req := new(CreateMessage)
 	err := c.BindJSON(req)
@@ -49,4 +56,46 @@ func (h *MessageHandler) CreateMessage(c *gin.Context) {
 	}
 
 	c.Status(http.StatusCreated)
+}
+
+// UpdateMessage _
+type UpdateMessage struct {
+	Content string `json:"content" binding:"min=1,max=300"`
+}
+
+// UpddateMessage _
+func (h *MessageHandler) UpdateMessage(c *gin.Context) {
+	req := UpdateMessage{}
+	err := c.BindJSON(&req)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+
+		return
+	}
+
+	id := c.Param("message_id")
+	messageId, err := strconv.Atoi(id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+
+		return
+	}
+
+	message := message.UpdateMessage{
+		Content: req.Content,
+	}
+	err = h.s.UpdateMessage(uint(messageId), message)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
